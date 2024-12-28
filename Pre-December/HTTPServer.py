61a39124c4e222fa813798f567276406926883cc
SybeDeBacker <sybe.debacker@student.uantwerpen.be>
2024-12-28 13:09:27 +0100

    Updated HTTPServer code to fit WSGI

--- Pre-December/HTTPServer.py
+++ Pre-December/HTTPServer.py
@@ -2,4 +2,4 @@ from PythonServer_Package import RobotObject, RobotServer
 
 robot = RobotObject()
 server = RobotServer(robot)
-server.run(host='127.0.0.1', port=80, debug=False)
+server.run(host='127.0.0.1', port=80)
