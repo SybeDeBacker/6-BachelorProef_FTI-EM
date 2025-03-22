4b006d9204bb19272f6c24afd39235c269cbcc11
SybeDeBacker <sybe.debacker@student.uantwerpen.be>
2025-03-22 21:19:46 +0100

    Update HTTPServer and User to use HTTPRobotControlAPI and change serial port configuration

--- 2e semester/HTTPServer.py
+++ 2e semester/HTTPServer.py
@@ -1,5 +1,5 @@
 from PythonServer_Package import RobotObject, RobotServer
 
-robot = RobotObject(serial_port='COM3', baud_rate=9600)
+robot = RobotObject(serial_port='COM7', baud_rate=9600)
 server = RobotServer(robot)
 server.run(host='127.0.0.1', port=80)
--- 2e semester/User.py
+++ 2e semester/User.py
@@ -1,4 +1,4 @@
-from Control_API import LocalRobotControlAPI as RobotControlAPI
+from Control_API import HTTPRobotControlAPI as RobotControlAPI
 
 # Example: Send a "MOVE" command
 if __name__ == "__main__":
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     ROBOT_IP = '10.0.1.250'  # Arduino's IP address (set in your Arduino sketch)
     ROBOT_URL = f"http://{ROBOT_IP}"
     # Connect to the robot server
-    Robot = RobotControlAPI(serial_port='COM7', baud_rate=9600)
+    Robot = RobotControlAPI(server_url=ROBOT_URL)
 
     # Start a loop to send commands and pings
     Robot.zero_robot()
